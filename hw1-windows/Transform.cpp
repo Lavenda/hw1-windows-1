7e0b2243e279d66850907f284634f856cc638adf
codes-bugs <32035323+codes-bugs@users.noreply.github.com>
2017-12-27 20:30:23 +0800

    ...

--- hw1-windows/Transform.cpp
+++ hw1-windows/Transform.cpp
@@ -24,7 +24,7 @@ void Transform::left(float degrees, vec3& eye, vec3& up) {
   // YOUR CODE FOR HW1 HERE
 	mat3 rmat3 = rotate(degrees, up);
 	eye = rmat3 * eye;
-	std::cout << "x:" << eye[0] << "y" << eye[1] << "z" << eye[2] << std::endl;
+	//std::cout << "x:" << eye[0] << "y" << eye[1] << "z" << eye[2] << std::endl;
 }
 
 // Transforms the camera up around the "crystal ball" interface
@@ -36,15 +36,30 @@ void Transform::up(float degrees, vec3& eye, vec3& up) {
 	up = glm::transpose(glm::inverse(rmat3))*up;
 	/*eye = mat3(1, 0, 0, 0, 0, -1, 0, 1, 0)*eye;
 	up = vec3(0, 0, -1);*/
-	std::cout<<"x:" << eye[0]<<" y:"<<eye[1]<<" z:"<<eye[2]<<"up:"<<up[0]<<" "<<up[1]<<" "<<up[2]<<std::endl;
+	//std::cout<<"x:" << eye[0]<<" y:"<<eye[1]<<" z:"<<eye[2]<<"up:"<<up[0]<<" "<<up[1]<<" "<<up[2]<<std::endl;
 }
 
 // Your implementation of the glm::lookAt matrix
 mat4 Transform::lookAt(vec3 eye, vec3 up) {
   // YOUR CODE FOR HW1 HERE
-
-  // You will change this return call
-  return mat4();
+	vec3 w = glm::normalize(eye);
+	vec3 u = glm::normalize(glm::cross(up, w));
+	vec3 v = glm::normalize(glm::cross(w, u));
+	mat4 r_mat4, t_mat4;
+	r_mat4[0][0] = u[0];
+	r_mat4[1][0] = u[1];
+	r_mat4[2][0] = u[2];
+	r_mat4[0][1] = v[0];
+	r_mat4[1][1] = v[1];
+	r_mat4[2][1] = v[2];
+	r_mat4[0][2] = w[0];
+	r_mat4[1][2] = w[1];
+	r_mat4[2][2] = w[2];
+	t_mat4[3][0] = -eye[0];
+	t_mat4[3][1] = -eye[1];
+	t_mat4[3][2] = -eye[2];
+	// You will change this return call
+	return r_mat4 * t_mat4;
 }
 
 Transform::Transform()
